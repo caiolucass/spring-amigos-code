7605a672f097da2bfbb04b4b64231eee43c7ba6c
Caio Lucas <caiolfsantoss@gmail.com>
2021-10-16 17:57:29 -0300

    Construção e mapeamento das entitys no banco Postgres

--- springCourse/src/main/java/com/example/springCourse/student/student.java
+++ springCourse/src/main/java/com/example/springCourse/student/student.java
@@ -1,9 +1,15 @@
 package com.example.springCourse.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class student {
 
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence",  allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
