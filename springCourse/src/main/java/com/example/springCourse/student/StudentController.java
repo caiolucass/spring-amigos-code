6f1081b23fccb331b75cd87a4b11f3186c1aed44
Caio Lucas <caiolfsantoss@gmail.com>
2021-10-20 21:01:00 -0300

    Implementação da camada Repository e inserção de dados no banco

--- springCourse/src/main/java/com/example/springCourse/student/StudentController.java
+++ springCourse/src/main/java/com/example/springCourse/student/StudentController.java
@@ -2,7 +2,6 @@ package com.example.springCourse.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
