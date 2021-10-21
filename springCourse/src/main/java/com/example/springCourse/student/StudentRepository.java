b7bd5102ed1be02680af8f960cd439c2df93f4bc
Caio Lucas <caiolfsantoss@gmail.com>
2021-10-20 21:14:51 -0300

    Correção de comentário

--- springCourse/src/main/java/com/example/springCourse/student/StudentRepository.java
+++ springCourse/src/main/java/com/example/springCourse/student/StudentRepository.java
@@ -3,7 +3,6 @@ package com.example.springCourse.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
