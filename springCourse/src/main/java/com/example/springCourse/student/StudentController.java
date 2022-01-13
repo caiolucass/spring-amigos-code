194adb3d09cd8ee6a1c8527fa1bfd8eab6484dfe
Caio Lucas <caiolfsantoss@gmail.com>
2022-01-13 16:52:22 -0300

    Criacao do service e controller do deleteStudent

--- springCourse/src/main/java/com/example/springCourse/student/StudentController.java
+++ springCourse/src/main/java/com/example/springCourse/student/StudentController.java
@@ -25,4 +25,9 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studenId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
 }
--- springCourse/src/main/java/com/example/springCourse/student/StudentService.java
+++ springCourse/src/main/java/com/example/springCourse/student/StudentService.java
@@ -33,4 +33,14 @@ public class StudentService {
         }
        studentRepository.save(student);
     }
+
+    //exclui um estudante pelo Id
+    public void deleteStudent(Long studentId){
+       boolean exists = studentRepository.existsById(studentId);
+
+       if(!exists){
+           throw new IllegalStateException("student with " + studentId + " does not exists");
+       }
+       studentRepository.deleteById(studentId);
+    }
 }
